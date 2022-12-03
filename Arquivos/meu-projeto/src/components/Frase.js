434886b3f90a005f2075b34ca73223b5a25a545a
Adriel Nazar Santos <99226370+adrielnazar@users.noreply.github.com>
2022-12-03 02:06:35 -0300

    Aula 9

--- Arquivos/meu-projeto/src/components/Frase.js
+++ Arquivos/meu-projeto/src/components/Frase.js
@@ -1,7 +1,9 @@
+import styles from "./Frase.module.css";
+
 function Frase() {
     return (
-        <div>
-            <p>Esse é um componente com uma frase!</p>
+        <div className={styles.fraseConteiner}>
+            <p className={styles.fraseContent}>Esse é um componente com uma frase!</p>
         </div>
     )
 }
